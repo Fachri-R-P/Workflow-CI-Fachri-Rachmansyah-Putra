998404101b08a3ad3ccb5ec1cbe47b5d185d9ef5
Fachri-R-P <fahry1008@gmail.com>
2025-12-23 08:02:11 +0700

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -28,7 +29,8 @@ with mlflow.start_run():
 
     y_pred = model.predict(X_test)
 
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
+    
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     r2 = r2_score(y_test, y_pred)
 
     print("RMSE:", rmse)
